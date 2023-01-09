82dc00938a4dd349a17771588aa351a552f9f27c
cauefrancisco <cauefranscis@gmail.com>
2023-01-09 12:15:17 -0300

    blog warning

--- src/pages/Blog/Blog.jsx
+++ src/pages/Blog/Blog.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { IoIosConstruct } from 'react-icons/io';
 import './Blog.css';
+
 
 const Blog = () => {
   return (
     <div className='blog'>
+        <div className="content">
+
+        <h1 className='titleBlog'>The Blog is in the works!</h1>
+        <p>Don't Worry, soon enough it will be here</p>
+        <IoIosConstruct className='iconWorking' />
       
+        </div>
 
     </div>
   )
