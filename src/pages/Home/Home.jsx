fae000ab31e43bdce249cbe90aa21008e69ec4b8
cauefrancisco <cauefranscis@gmail.com>
2023-01-12 19:05:47 -0300

    update

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,24 +1,59 @@
 import React from 'react';
-import Profile from './../../assets/profile.png';
+import TypeWriterEffect from 'react-typewriter-effect';
+// import Profile from './../../assets/profile.png';
 import './Home.css';
 
+
 export default function Home() {
+
   return (
     <div className='home'>
       <div className="row">
 
         <div className="board">
           <div className="text-board">
-            <h3 className='board-title'>Hello World,</h3>
-            <p className='board-text'>My name is <span>Caue Francisco</span>. <br />
-             I'm a <span>front-end</span> developer. <br /> <span>This</span> is my space!
-            </p>
+
+          <TypeWriterEffect
+          className='typewriter'
+        textStyle={{
+          fontWeight: 500,
+          fontSize: '10dvw',
+          color: `#EBECE8`,
+        }}
+        startDelay={2000}
+        cursorColor="#3F3D56"
+        multiText={[
+          'Hello World,',
+          `My name is Caue Francisco`,
+          "I'm a front-end developer",
+        ]}
+        multiTextDelay={1500}
+        typeSpeed={50}
+        multiTextLoop={true}
+      />
           </div>
+        {/* <div className="profile-pic">
+          <img className='profile' src={Profile} alt="profile" />
+        </div> */}
         </div>
 
-        <div className="profile-pic">
-          <img className='profile' src={Profile} alt="profile" />
-        </div>
+
+        {/* <TypeWriterEffect
+        textStyle={{
+          fontWeight: 500,
+          fontSize: '2dvw',
+        }}
+        startDelay={2000}
+        cursorColor="#3F3D56"
+        multiText={[
+          'Hello World,',
+          'My name is Caue Francisco',
+          "I'm a front-end developer",
+        ]}
+        multiTextDelay={1500}
+        typeSpeed={50}
+        multiTextLoop={true}
+      /> */}
 
       </div>
     </div>
